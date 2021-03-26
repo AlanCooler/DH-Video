b505c21b42f6a956257ab1d32b42cbd270ddbac3
demonizze <artem@securityarea.com>
2021-03-26 19:20:17 +0300

    Инициализация нового C# WPF проекта

---
+++ DH-Video/DH-Video/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DH_Video
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ DH-Video/DH-Video/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace DH_Video
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
