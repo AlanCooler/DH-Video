bcba38f5725773ecdd1b7645be82c0f1dfee4ff8
demonizze <artem@securityarea.com>
2021-03-26 19:33:13 +0300

    Тестирование базовой реализации паттерна MVVM в WPF проекте

---
+++ DH-Video/DH-Video/Libs/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DH_Video.Annotations;
+
+namespace DH_Video.Libs
+{
+    /// <summary>
+    /// Basic ViewModel of pattern MVVM
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Basic PropertyChangedEvent
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Tell view that value in the viewmodel was changed. Realization basic PropertyChange event.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DH-Video/DH-Video/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *  Musin Artem
+ *
+ *   * * * DH-Video * * *
+ * 
+ *  2020 Copyright  
+ */
+
+using DH_Video.Libs;
+
+namespace DH_Video.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The name of video which is playing right now
+        /// </summary>
+        public string CurrentVideoName
+        {
+            get => _currentVideoName;
+            set
+            {
+                _currentVideoName = value;
+                // Change this value
+                OnPropertyChanged(CurrentVideoName);
+            }
+        }
+        private string _currentVideoName = "DH Video";
+
+        
+    }
+}
